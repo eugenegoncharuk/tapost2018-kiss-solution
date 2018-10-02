95ddccd4597e067c844091d1b31f69a4ea55b64c
Martinsh Gailums <Martinsh_Gailums@rcomext.com>
2018-10-02 16:10:06 +0300

    Shopping Cart test adjusted

--- src/test/java/com/shoppingcart/ShoppingCartTest.java
+++ src/test/java/com/shoppingcart/ShoppingCartTest.java
@@ -136,8 +136,8 @@ public class ShoppingCartTest extends FrameworkTest {
         cartPage.getGiftVoucherSubmitButton().click();
         cartPage.pause(2L);
 
-        assertEquals("20.00€", cartPage.getProductPriceSum().getText(), "Verify shopping cart Label");
-        assertTrue(cartPage.getShoppingCart().getText().contains("1 item(s) - 20.00€"), "Verify shopping cart Label");
+        assertEquals("30.00€", cartPage.getProductPriceSum().getText(), "Verify shopping cart Label");
+        assertTrue(cartPage.getShoppingCart().getText().contains("1 item(s) - 30.00€"), "Verify shopping cart Label");
     }
 
 }
